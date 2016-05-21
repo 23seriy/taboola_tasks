9c1403dc1239106b03aff90beae4fbefbb7671bd
NICIJ <jenkins@naturalint.com>
2016-05-21 20:56:35 +0300

    fixes

--- task2/taboola_second_task.py
+++ task2/taboola_second_task.py
@@ -4,32 +4,13 @@ from collections import Counter
 
 f1 = open("balancer-access.log","r")
 lines = f1.readlines()
-print (lines)
 f1.close()
 
-f2 = open("click_lines", "w")
+ips = []
 for line in lines:
   if "click" in line:
-    f2.write(line)    
-f2.close()
-
-
-f3 = open("click_lines", "r")
-clicks = f3.readlines()
-f2.close()
-
-
-f4 = open("ips_lines", "w")
-for line in clicks:
-  f4.write(line.split()[0]+'\n')
-f4.close()
-
-f5 = open("ips_lines", "r")
-sorted = f5.readlines()
-sorted.sort()
-print "print sorted ips:"
-print (sorted)
-
-word_counts = Counter(sorted)
-top = word_counts.most_common(1)
-print(top)
+     ips.append(line.split()[0])				
+			 
+ips.sort()
+top = Counter(ips).most_common(1)
+print(top[0][0])
